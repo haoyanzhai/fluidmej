25b72d85070b3606c8b294f281f985b140953837
houmengxue <houmengxue@gmail.com>
2019-01-05 18:42:26 -0500

    Add files via upload

--- new/find_region_via_boundary_transect.m
+++ new/find_region_via_boundary_transect.m
@@ -22,19 +22,21 @@ function region_no = find_region_via_boundary_transect(boundaries, boundary_pt_x
 
 field_names = fieldnames(boundaries);
 boundary_name = [];
-
+region_no = [];
 for ii = 1:numel(field_names)
     current_boundary = getfield(boundaries, field_names{ii});
     for jj = 1:size(current_boundary,1)-1
         p_point = current_boundary(jj,:);
         n_point = current_boundary(jj+1,:);
         if (n_point(1) - boundary_pt_x)/(n_point(2) - boundary_pt_y) ==  (p_point(1) - boundary_pt_x)/(p_point(2) - boundary_pt_y)
-            if isempty(boundary_name)
+            %if isempty(boundary_name)
                 boundary_name = field_names(ii);
-            else
-                error(['The point ', num2str(boundary_pt_x), ', ', num2str(boundary_pt_y), ' is on multiple boundaries.']);
-            end
-            region_no = [str2num(boundary_name{1}(end)), str2num(boundary_name{1}(end-1))]; % ascend order
+            %else
+            %    error(['The point ', num2str(boundary_pt_x), ', ', num2str(boundary_pt_y), ' is on multiple boundaries.']);
+            %end
+            region_no = [region_no, str2num(boundary_name{1}(end)), str2num(boundary_name{1}(end-1))]; 
         end
     end
 end
+region_no = sort(unique(region_no), 'ascend');
+
